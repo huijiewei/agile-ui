4765c2e3f89f7fbe8d84c7a9faa02a95d48ea282
Huijie Wei <huijiewei@outlook.com>
2022-06-17 11:22:17 +0800

    修复文档生成

--- packages/utils/src/string/string.ts
+++ packages/utils/src/string/string.ts
@@ -1,8 +1,14 @@
+/* 驼峰命名法，第一个单词首字母小写，后面的每个单词首字母大写，又名小驼峰命名法。 */
 export const camelCase = (str: string): string =>
   str.trim().replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''));
 
+/* 帕斯卡命名法，每个单词首字母大写，又名大驼峰命名法*/
 export const pascalCase = (str: string): string =>
   (str.match(/[a-zA-Z0-9]+/g) || []).map((w) => `${w.charAt(0).toUpperCase()}${w.slice(1)}`).join('');
+
+/* 短横线隔开命名法，每个单词首字母小写。 */
+export const kebabCase = (str: string): string =>
+  str.replace(/\B([A-Z])(?=[a-z])|(?<=[a-z0-9])([A-Z])/g, '-$1$2').toLowerCase();
 
 export const slugify = (str: string): string => {
   return str
--- website/scripts/contentlayer-helper.ts
+++ website/scripts/contentlayer-helper.ts
@@ -1,12 +1,7 @@
 import { resolve } from 'path';
 
-const slugify = (str: string): string =>
-  str
-    .normalize('NFKD')
-    .toLowerCase()
-    .replace(/[^\w\s-]/g, '')
-    .trim()
-    .replace(/[-\s]+/g, '-');
+const kebabCase = (str: string): string =>
+  str.replace(/\B([A-Z])(?=[a-z])|(?<=[a-z0-9])([A-Z])/g, '-$1$2').toLowerCase();
 
 const gitHubUrl = 'https://github.com/huijiewei/agile-ui/blob/main';
 
@@ -15,7 +10,7 @@ export const getComponentName = (filename: string) => {
 };
 
 export const getComponentPathName = (componentName: string) => {
-  const slug = slugify(componentName);
+  const slug = kebabCase(componentName);
 
   return resolve(`../packages/react/src/${slug}/${componentName}.tsx`);
 };
@@ -27,6 +22,6 @@ export const getComponentDocLink = (filename: string) => {
 
 export const getComponentSourceLink = (filename: string) => {
   const name = getComponentName(filename);
-  const slug = slugify(name);
+  const slug = kebabCase(name);
   return `${gitHubUrl}/packages/react/src/${slug}/${name}.tsx`;
 };
