f178e3896527df7d1ae9201396563a8e563afb38
Huijie Wei <huijiewei@outlook.com>
2022-07-09 23:23:23 +0800

    feat: 优化 useLocalStorage 实现

--- packages/react-hooks/src/use-local-storage/index.ts
+++ packages/react-hooks/src/use-local-storage/index.ts
@@ -1,47 +1,47 @@
-import { isBrowser, runIfFn } from '@agile-ui/utils';
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { MaybeFunction, runIfFn } from '@agile-ui/utils';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { useEventListener } from '../use-event-listener';
 
-export const useLocalStorage = <T>(name: string, initialValue: (() => T) | T): [T, Dispatch<SetStateAction<T>>] => {
-  const [state, setState] = useState<T>(() => {
+export const useLocalStorage = <T>(key: string, initialValue: MaybeFunction<T>): [T, Dispatch<SetStateAction<T>>] => {
+  const readValue = () => {
     try {
-      const item = window.localStorage.getItem(name);
+      const item = window.localStorage.getItem(key);
 
       if (item != null) {
         return JSON.parse(item);
       }
     } catch (error) {
-      console.warn(`Error reading localStorage key “${name}”:`, error);
+      console.warn(`Error reading localStorage key “${key}”:`, error);
     }
 
     return runIfFn(initialValue);
-  });
+  };
 
-  useEffect(() => {
-    if (!isBrowser()) {
-      return;
-    }
+  const [state, setState] = useState<T>(readValue);
+
+  const setValue: Dispatch<SetStateAction<T>> = (value) => {
+    const newValue = runIfFn(value, state);
+
+    setState(newValue);
 
     try {
-      if (state != undefined) {
-        window.localStorage.setItem(name, JSON.stringify(state));
+      if (newValue != undefined) {
+        window.localStorage.setItem(key, JSON.stringify(newValue));
       } else {
-        window.localStorage.removeItem(name);
+        window.localStorage.removeItem(key);
       }
     } catch (error) {
-      console.warn(`Error writing localStorage key “${name}”:`, error);
+      console.warn(`Error writing localStorage key “${key}”:`, error);
     }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state]);
+  };
 
   useEventListener('storage', (e: StorageEvent) => {
-    if (e.key !== name || e.storageArea !== window.localStorage) {
+    if (e.key !== key || e.storageArea !== window.localStorage) {
       return;
     }
 
     setState(e.newValue ? JSON.parse(e.newValue) : undefined);
   });
 
-  return [state, setState];
+  return [state, setValue];
 };
