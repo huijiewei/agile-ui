546e959c666538e2b443b8a8d850a39e2d428fbe
Huijie Wei <huijiewei@outlook.com>
2022-07-26 18:32:42 +0800

    chore: 代码高亮主题统一, 修改文字错误

--- website/src/components/playground/PlaygroundCode.tsx
+++ website/src/components/playground/PlaygroundCode.tsx
@@ -1,15 +1,15 @@
 import { useColorModeState } from '@agile-ui/react';
 import { CodeBlock } from '../code/CodeBlock';
 import { CopyIcon } from '../code/CopyIcon';
-import vsLight from 'prism-react-renderer/themes/vsLight';
-import vsDark from 'prism-react-renderer/themes/vsDark';
+import darkTheme from 'prism-react-renderer/themes/nightOwl';
+import lightTheme from 'prism-react-renderer/themes/nightOwlLight';
 
 export const PlaygroundCode = ({ code }: { code: string }) => {
   const { darkMode } = useColorModeState();
 
   return (
     <div className={'relative'}>
-      <CodeBlock disabled code={code} theme={darkMode ? vsDark : vsLight} language={'tsx'} />
+      <CodeBlock disabled code={code} theme={darkMode ? darkTheme : lightTheme} language={'tsx'} />
       <CopyIcon content={code} />
     </div>
   );
