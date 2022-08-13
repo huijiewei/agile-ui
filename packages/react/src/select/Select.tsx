68f3e06bb83ef90abbd4783bdbd78f5bce53e340
Huijie Wei <huijiewei@outlook.com>
2022-08-13 14:57:00 +0800

    fix: 修复 Select 大数据虚拟列表滚动位置问题

--- packages/react/src/select/Select.tsx
+++ packages/react/src/select/Select.tsx
@@ -118,26 +118,31 @@ const selectSizes = {
     base: 'py-px text-sm',
     select: 'pl-2 pr-1 gap-2',
     option: 'px-2',
+    height: 22,
   },
   sm: {
     base: 'py-0.5',
     select: 'pl-2 pr-1 gap-2',
     option: 'px-2',
+    height: 26,
   },
   md: {
     base: 'py-1',
     select: 'pl-3 pr-1.5 gap-3',
     option: 'px-3',
+    height: 30,
   },
   lg: {
     base: 'py-1.5',
     select: 'pl-3 pr-1.5 gap-3',
     option: 'px-3',
+    height: 34,
   },
   xl: {
     base: 'py-[7px] text-lg',
     select: 'pl-3 pr-1.5 gap-3',
     option: 'px-3',
+    height: 38,
   },
 };
 
@@ -352,10 +357,12 @@ export const Select = primitiveComponent<'input', SelectProps>((props, ref) => {
     }),
   ]);
 
+  const sizeClass = selectSizes[size];
+
   const rowVirtual = useVirtualizer({
     count: elements.length,
     getScrollElement: () => refs.floating.current,
-    estimateSize: () => 32,
+    estimateSize: () => sizeClass.height,
     overscan: 3,
     enableSmoothScroll: false,
     paddingStart: 6,
@@ -459,8 +466,6 @@ export const Select = primitiveComponent<'input', SelectProps>((props, ref) => {
       }
     },
   });
-
-  const sizeClass = selectSizes[size];
 
   return (
     <SelectProvider
@@ -599,6 +604,7 @@ export const Select = primitiveComponent<'input', SelectProps>((props, ref) => {
                             position: 'absolute',
                             top: 0,
                             left: 0,
+                            height: `${row.size}px`,
                             transform: `translateY(${row.start}px)`,
                           },
                         })
