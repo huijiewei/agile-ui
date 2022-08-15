9a3561a35ccfbc6ed74f41d124ef05aa18104297
Huijie Wei <huijiewei@outlook.com>
2022-08-15 09:53:28 +0800

    chore: 更新 ESLint 规则, 允许 _ 开始的变量未使用

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,15 @@ module.exports = {
         'prettier',
       ],
       rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+          },
+        ],
         'react/prop-types': 'off',
       },
     },
