c51835637e20f765ac4ccd983fb4465e65625285
Huijie Wei <huijiewei@outlook.com>
2022-08-21 17:54:18 +0800

    test: 增加 Mock

--- tooling/test-utils/src/index.ts
+++ tooling/test-utils/src/index.ts
@@ -1,3 +1,9 @@
 export * from './react/render';
 export * from './react/renderHook';
 export * from './a11y/a11y';
+
+import { mockImage } from './mocks/image';
+
+export const mocks = {
+  image: mockImage,
+};
