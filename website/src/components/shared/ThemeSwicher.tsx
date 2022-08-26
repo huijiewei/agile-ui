4ea434e25412524ea3afe0c6003ada16f06004e1
Huijie Wei <huijiewei@outlook.com>
2022-08-26 08:44:20 +0800

    docs: 颜色切换模式使用菜单

--- website/src/components/shared/ThemeSwicher.tsx
+++ website/src/components/shared/ThemeSwicher.tsx
@@ -1,9 +1,18 @@
-import { Tooltip, useColorModeDispatch, useColorModeState } from '@agile-ui/react';
+import {
+  DropdownMenu,
+  DropdownMenuArrow,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  useColorModeDispatch,
+  useColorModeState,
+} from '@agile-ui/react';
 import { Moon, Sun, System } from '@agile-ui/react-icons';
+import { cx } from 'twind';
 
 export const ThemeSwitcher = () => {
   const { colorMode } = useColorModeState();
-  const toggleColorMode = useColorModeDispatch();
+  const { setColorMode } = useColorModeDispatch();
 
   const colorModeTitle = {
     dark: '黑暗模式',
@@ -12,16 +21,50 @@ export const ThemeSwitcher = () => {
   };
 
   return (
-    <Tooltip placement={'bottom'} content={colorModeTitle[colorMode]}>
-      <button
-        aria-label={colorModeTitle[colorMode]}
-        onClick={() => toggleColorMode()}
-        className={'block select-none transition-colors p-1 text-gray-400 hover:text-gray-500'}
-      >
-        {colorMode == 'system' && <System size={5} />}
-        {colorMode == 'light' && <Sun size={5} />}
-        {colorMode == 'dark' && <Moon size={5} />}
-      </button>
-    </Tooltip>
+    <DropdownMenu placement={'bottom'}>
+      <DropdownMenuTrigger>
+        <button
+          aria-label={colorModeTitle[colorMode]}
+          className={'block select-none transition-colors p-1 text-gray-400 hover:text-gray-500'}
+        >
+          {colorMode == 'system' && <System size={5} />}
+          {colorMode == 'light' && <Sun size={5} />}
+          {colorMode == 'dark' && <Moon size={5} />}
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className={'z-50'}>
+        <DropdownMenuArrow />
+        <DropdownMenuItem
+          className={cx('my-1', colorMode == 'light' && 'text-blue-500')}
+          onClick={() => {
+            setColorMode('light');
+          }}
+        >
+          <div className={'flex gap-1 items-center'}>
+            <Sun /> 亮色模式
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className={cx('my-1', colorMode == 'dark' && 'text-blue-500')}
+          onClick={() => {
+            setColorMode('dark');
+          }}
+        >
+          <div className={'flex gap-1 items-center'}>
+            <Moon /> 暗色模式
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className={cx('my-1', colorMode == 'system' && 'text-blue-500')}
+          onClick={() => {
+            setColorMode('system');
+          }}
+        >
+          <div className={'flex gap-1 items-center'}>
+            <System /> 跟随系统
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
